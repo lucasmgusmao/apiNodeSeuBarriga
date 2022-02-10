4b1f20f6db2d58be7af429813364121e6736e416
unknown <lucas_monteiro@msn.com>
2022-02-10 16:21:31 -0300

    Geracao de seed de massa de dados.

--- knexfile.js
+++ knexfile.js
@@ -10,6 +10,10 @@ module.exports = {
       },
       migrations: {
          directory: './src/migrations'
-      }      
+      },
+      seeds: {
+         directory: './src/seeds'
+      }
+
    }
 }
